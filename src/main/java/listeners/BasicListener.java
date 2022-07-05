8f75068decd6fea93b812861e845347ef5668773
khvanches <khvanches@gmail.com>
2022-07-05 18:09:46 +0300

    Rework selenium listener to JS

--- src/main/java/listeners/BasicListener.java
+++ src/main/java/listeners/BasicListener.java
@@ -4,12 +4,10 @@ import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.events.WebDriverListener;
-import waiters.StandardWaiter;
 
 
 public class BasicListener implements WebDriverListener {
   private WebDriver driver;
-  private StandardWaiter waiter = new StandardWaiter(driver);
   private String color;
 
   public BasicListener(WebDriver driver){
@@ -19,13 +17,9 @@ public class BasicListener implements WebDriverListener {
   @Override
   public void beforeClick(WebElement element) {
     ((JavascriptExecutor) driver).executeScript(
-      String.format("arguments[0].setAttribute('style', 'outline: 4px solid %s;');", color), element);
-  }
-
-  @Override
-  public void afterClick(WebElement element) {
-    ((JavascriptExecutor)driver).executeScript(
-      String.format("arguments[0].removeAttribute('style', 'outline: 4px solid %s;');", color), element);
+        String.format("arguments[0].setAttribute('style', 'outline: 4px solid %s;');", color), element);
+    ((JavascriptExecutor) driver).executeScript(
+        "arguments[0].addEventListener(\"click\", () => {arguments[0].removeAttribute('style', 'outline');} )", element);
   }
 
   public void setColor(String color) {
